1a5765f6f0fbaf3dbd2b9e4c279bc8d35b409884
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2023-03-02 16:02:12 +0100

    add new template and add template support to the create sh

--- .scripts/create-go.sh
+++ .scripts/create-go.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+scriptDir=$(dirname $(readlink -fn $0))
+templateDir=$(dirname $scriptDir)/.templates/go
+
+templates=( $(ls -1 $templateDir) )
+
 if [[ ! $# -eq 1 ]]; then
-    echo "please provide a project description like 'symmetric-difference'"
+    echo "please provide a project template like '${templates[0]}'"
+    echo ""
+    echo "this is a list of possible go templates:"
+    for template in ${templates[@]}; do
+        echo " * $template"
+    done
+    exit 1
+fi
+
+chosenTemplate=$1
+
+if [[ ! " ${templates[@]} " =~ " $chosenTemplate " ]]; then
+    echo "template '$chosenTemplate' does not exist"
+    echo ""
+    echo "this is a list of possible go templates:"
+    for template in ${templates[@]}; do
+        echo " * $template"
+    done
     exit 1
 fi
 
@@ -12,11 +34,13 @@ fi
 
 root=$(dirname $(dirname $(readlink -fn $0)))
 dir="$(date +%Y-%m-%d)-$1"
+projectDir="$root/$dir"
 
 echo "creating new project dir:"
-echo "  $root/$dir"
+echo "  $projectDir"
 
-mkdir -p "$root/$dir"
-cd "$root/$dir"
+mkdir -p "$projectDir"
+cp -r "$templateDir/$chosenTemplate/." "$projectDir"
+cd "$projectDir"
 go mod init "github.com/svenliebig/coding-puzzles/$dir"
 echo "done!"
