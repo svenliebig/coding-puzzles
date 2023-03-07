b5c530edb0c4ff88e042a05b0226d172a5e06996
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2023-03-07 11:40:37 +0100

    resolve todays coding task

--- 2023-03-07-jump-game-go/main.go
+++ 2023-03-07-jump-game-go/main.go
@@ -1,5 +1,56 @@
 package main
 
 func minJumps(arr []int) int {
-  return 0
+  if len(arr) == 1 {
+    return 0
+  }
+
+  graph := make(map[int][]int)
+  for i, v := range arr[1:] {
+    if gv, ok := graph[v]; ok {
+      graph[v] = append(gv, i + 1)
+    } else {
+      graph[v] = []int{i + 1}
+    }
+  }
+
+  queue := make([]int, 1, len(arr))
+  queue[0] = 0
+  step := 0
+  visited := make([]bool, len(arr))
+  visited[0] = true
+  
+  for len(queue) > 0 {
+    cl := len(queue)
+
+    for _, v := range queue[:cl] {
+
+      if v == len(arr) - 1 {
+        return step
+      }
+
+      for _, gv := range graph[arr[v]] {
+        if !visited[gv] {
+          queue = append(queue, gv)
+          visited[gv] = true
+          delete(graph, arr[v])
+        }
+      }
+
+      if v > 0 && !visited[v - 1] {
+        queue = append(queue, v - 1)
+        visited[v - 1] = true
+      }
+
+      if !visited[v + 1] {
+        queue = append(queue, v + 1)
+        visited[v + 1] = true
+      }
+    }
+
+    step++
+    queue = queue[1:]
+  }
+
+  return len(arr)
 }
